0e39945a451a203014338a4f152b4161d1637d38
Juliano <user@email.com>
2018-09-28 22:33:08 -0300

    Alteracao de diretorio

--- CRL_Server.sh
+++ CRL_Server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ./jiacerts-ca/LCR
+cd ./ca-authority/CRL
 sudo python3 -m http.server 80
--- OCSP_Server.sh
+++ OCSP_Server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl ocsp -index jiacerts-ca/conf/index -port 8888 -rsigner ocsp.cer -rkey ocsp.key -CA cacert.pem -text -out log.txt
+openssl ocsp -index ca-authority/conf/index -port 8888 -rsigner ocsp.cer -rkey ocsp.key -CA cacert.pem -text -out log.txt
